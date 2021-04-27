b074c5579c7afa55199c8bfcca0a1704b1bf4e63
elincreator2126 <elincreator2126@gmail.com>
2021-04-27 14:34:43 +0900

    gitproject

---
+++ gitproject/src/test/java/githubtest/GitprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package githubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
